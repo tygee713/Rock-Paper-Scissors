dbd80ab094b5ac5c8c8ed40095fefb9257e884dd
Tyler Geerdts <tgeerdts@Tylers-MacBook-Pro.local>
2016-09-18 15:01:33 -0400

    Basic HTML layout and styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log("loaded");
+  var config = {
+    apiKey: "AIzaSyAfg-hWMpR6blUTSKVp-A4mxoyMGNNmpGY",
+    authDomain: "rockpaperscissors-8cd70.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-8cd70.firebaseio.com",
+    storageBucket: "rockpaperscissors-8cd70.appspot.com",
+    messagingSenderId: "979124762446"
+  };
+  firebase.initializeApp(config);
