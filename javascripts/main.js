d731e606d049d2d7e4a23bf6958426eb5e7124dc
Tyler Geerdts <tgeerdts@Tylers-MacBook-Pro.local>
2016-09-21 12:42:49 -0400

    attempted user info deletion on disconnect, fixed indentation, added comments

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,165 +1,149 @@
-// $(document).ready(function() {
-//   playerNum = 0;
-
-// });
-
 var config = {
-      apiKey: "AIzaSyAfg-hWMpR6blUTSKVp-A4mxoyMGNNmpGY",
-      authDomain: "rockpaperscissors-8cd70.firebaseapp.com",
-      databaseURL: "https://rockpaperscissors-8cd70.firebaseio.com",
-      storageBucket: "rockpaperscissors-8cd70.appspot.com",
-      messagingSenderId: "979124762446"
-    };
-    firebase.initializeApp(config);
+  apiKey: "AIzaSyAfg-hWMpR6blUTSKVp-A4mxoyMGNNmpGY",
+  authDomain: "rockpaperscissors-8cd70.firebaseapp.com",
+  databaseURL: "https://rockpaperscissors-8cd70.firebaseio.com",
+  storageBucket: "rockpaperscissors-8cd70.appspot.com",
+  messagingSenderId: "979124762446"
+};
+firebase.initializeApp(config);
 
-    var database = firebase.database();
-    var p1Pick = "";
-    var p2Pick = "";
-    var numPlayers = 0;
-    var playerNum = 0;
-    var hasTwoPlayers = false;
-    var p1Wins = 0;
-    var p1Losses = 0;
-    var players = [];
-    var turn = 1;
+var database = firebase.database();
+var p1Pick = "";
+var p2Pick = "";
+var numPlayers = 0;
+var playerNum = 0;
+var hasTwoPlayers = false;
+var p1Wins = 0;
+var p1Losses = 0;
+var p2Wins = 0;
+var p2Losses = 0;
+var players = [];
+var turn = 1;
+var playerRef;
 
-  $("#start").on('click', function() {
-    if (numPlayers < 2) {
-      playerNum = numPlayers + 1;
-      var player = database.ref('/players').child(playerNum);
-      players.push(player);
-      console.log("player " + playerNum + " is currently playing");
-      $("#p" + playerNum + "Buttons").show();
-      player.set({ 
-        name: $("#userName").val().trim(),
-        wins: p1Wins,
-        losses: p1Losses
-      });
-      var setTurn = database.ref().child('turn');
-      setTurn.set(turn);
-    }
-    else {
-      alert("Two players are already playing!");
-    }
-    
-    return false;
-  });
+//starts the game for each player
+$("#start").on('click', function() {
 
-var connectionsRef = database.ref("/connections");
+  //checks if two users are already playing
+  if (numPlayers < 2) {
+    playerNum = numPlayers + 1;
+    player = database.ref('/players').child(playerNum);
+    players.push(player);
 
-// '.info/connected' is a special location provided by Firebase that is updated every time the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+    //displays the correct player's buttons
+    $("#p" + playerNum + "Buttons").show();
+    player.set({ 
+      name: $("#userName").val().trim(),
+      wins: '0',
+      losses: '0'
+    });
+    var setTurn = database.ref().child('turn');
+    setTurn.set(turn);
+
+    playerRef = database.ref('/players/' + playerNum);
+  }
+  else {
+    alert("Two players are already playing!");
+  }
+  
+  return false;
+});
+
+
+var connectionsRef = database.ref("/connectedData");
+
 var connectedRef = database.ref(".info/connected");
 
-// When the client's connection state changes...
+//adds and removed connected data to a separate table, NEED TO REMOVE USER DATA
 connectedRef.on("value", function(snap) {
-
-  // If they are connected..
-  if( snap.val() ) {
-
-    // Add user to the connections list.
+  
+  if(snap.val()) {
     var con = connectionsRef.push(true);
-
-    // Remove user from the connection list when they disconnect.
+    // var connected = connectionsRef.child(playerNum);
     con.onDisconnect().remove();
-
   };
 
 });
 
-// When first loaded or when the connections list changes...
-connectionsRef.on("value", function(snap) {
-
-  // Display the viewer count in the html.
-  // The number of online users is the number of children in the connections list.
-  $("#connectedViewers").html(snap.numChildren());
-
+//attempting to remove the user data when a connected child gets removed, NOT WORKING
+connectionsRef.on("child_removed", function(snap) {
+  playerRef.remove();
 });
 
-
+//runs anytime the database is updated
 database.ref().on("value", function(snapshot) {
-      console.log("database updated");
-      numPlayers = snapshot.child("players").numChildren();
-      console.log("currently playing: " + numPlayers);
-      var pick1Snap = snapshot.child("players").child('1').child('pick');
-      var pick2Snap = snapshot.child("players").child('2').child('pick');
+  numPlayers = snapshot.child("players").numChildren();
 
-    if (pick1Snap.exists()) {
-            p1Pick = snapshot.child("players").child('1').val().pick;
-            console.log("player 1 picked: " + p1Pick);
-         }
-    if (pick2Snap.exists()) {
-            p2Pick = snapshot.child("players").child('2').val().pick;
-            console.log("player 2 picked: " + p2Pick);
-         }
-    if (pick1Snap.exists() && pick2Snap.exists()) {
-        comparePicks();
-    }
+  var pick1Snap = snapshot.child("players").child('1').child('pick');
+  var pick2Snap = snapshot.child("players").child('2').child('pick');
 
-    }, function (errorObject) {
-      // 
-        console.log("The read failed: " + errorObject.code);
-  });
+  //once the player's picks are in the database, store them locally
+  if (pick1Snap.exists()) {
+    p1Pick = snapshot.child("players").child('1').val().pick;
+  }
 
-// database.ref('/players').on("value", function(snapshot) {
-  
-//   });
+  if (pick2Snap.exists()) {
+    p2Pick = snapshot.child("players").child('2').val().pick;
+  }
 
-  $(".optionsP1").on('click', function() {
-    p1Pick = $(this).data('pick');
-    var pick = database.ref('/players').child('1');
-    pick.update({pick: p1Pick});
-    console.log(p1Pick);
-    // if (p1Pick != "" && p2Pick != "") {
-    //   comparePicks();
-    // }
-    turn++;
-    var setTurn = database.ref().child('turn');
-      setTurn.set(turn);
-  });
+  //once both players have picked, compare the picks and determine the winner
+  if (pick1Snap.exists() && pick2Snap.exists()) {
+    comparePicks();
+  }
 
+  }, function (errorObject) {
+    // 
+      console.log("The read failed: " + errorObject.code);
+});
 
-  $(".optionsP2").on('click', function() {
-    p2Pick = $(this).data('pick');
-    var pick = database.ref('/players').child('2');
-    pick.update({pick: p2Pick});
-    console.log(p2Pick);
-    // if (p1Pick != "" && p2Pick != "") {
-    //   comparePicks();
-    // }
-    turn = 1;
-    var setTurn = database.ref().child('turn');
-      setTurn.set(turn);
-  });
+//when any of the player 1 buttons get clicked
+$(".optionsP1").on('click', function() {
+  p1Pick = $(this).data('pick');
+  var pick = database.ref('/players').child('1');
+  pick.update({pick: p1Pick});
+  turn = 2;
+  var setTurn = database.ref().child('turn');
+    setTurn.set(turn);
+});
 
+//when any of the player 2 buttons get clicked
+$(".optionsP2").on('click', function() {
+  p2Pick = $(this).data('pick');
+  var pick = database.ref('/players').child('2');
+  pick.update({pick: p2Pick});
+  turn = 1;
+  var setTurn = database.ref().child('turn');
+    setTurn.set(turn);
+});
 
+//determine a winner
 function comparePicks() {
-    if (p1Pick == p2Pick) {
-      $("#playerWin").html('Tie');
-    }
-      else if (p1Pick == 'paper' && p2Pick == 'scissors') {
+  if (p1Pick == p2Pick) {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'paper' && p2Pick == 'scissors') {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'scissors' && p2Pick == 'paper') {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'paper' && p2Pick == 'rock') {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'rock' && p2Pick == 'paper') {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'rock' && p2Pick == 'scissors') {
+    $("#playerWin").html('Tie');
+  }
+  else if (p1Pick == 'scissors' && p2Pick == 'rock') {
+    $("#playerWin").html('Tie');
+  }
 
-    }
-      else if (p1Pick == 'scissors' && p2Pick == 'paper') {
-
-    }
-      else if (p1Pick == 'paper' && p2Pick == 'rock') {
-
-    }
-      else if (p1Pick == 'rock' && p2Pick == 'paper') {
-
-    }
-      else if (p1Pick == 'rock' && p2Pick == 'scissors') {
-
-    }
-      else if (p1Pick == 'scissors' && p2Pick == 'rock') {
-
-    }
-
-    clearPicks();
+  clearPicks();
 }
 
-
+//after the picks have been compared, they get cleared both locally and on the database
 function clearPicks() {
   p1Pick = "";
   p2Pick = "";
